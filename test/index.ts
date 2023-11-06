5a7bf15baa299204455eff4adb7417ff8d9d844d
mathcovax <campani.mathieu@gmail.com>
2023-11-06 13:37:30 +0000

    [feat] testing

---
+++ test/index.ts
@@ -0,0 +1,9 @@
+
+import {workersTesting} from "@duplojs/worker-testing";
+
+workersTesting(
+	(path) => import(path),
+	__dirname + "/global",
+	__dirname + "/local",
+	// __dirname + "/mergeAbstractRoute",
+);
